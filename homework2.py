ade222f59392d1a81c05d44bd11df4cf6c3bd777
DeAleon <donnotel@mail.ru>
2024-06-29 12:52:01 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+all_quest = 12
+all_time = 1.5
+name_kurs = 'Python'
+time_qiest = (all_time / all_quest)
+print('Курс: ', name_kurs, ', всего задач:', all_quest, ', затрачено часов: ', all_time, ', среднее время выполнения ', time_qiest, ' часа.')
